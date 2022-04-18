b2cae39560697a609a933ea952b15bf39804df83
Baquiin <baquiinsamp@gmail.com>
2022-04-18 03:28:37 -0300

    third commit

--- js/mobile-navbar.js
+++ js/mobile-navbar.js
@@ -2,26 +2,27 @@ class MobileNavbar {
     constructor(mobileMenu, navList, navLinks) {
         this.mobileMenu = document.querySelector(mobileMenu);
         this.navList = document.querySelector(navList);
-        this.navLinks = document.querySelector(navLinks);
+        this.navLinks = document.querySelectorAll(navLinks);
         this.activeClass = "active";
         this.handleClick = this.handleClick.bind(this);
     }
-    animeteLinks() {
+    animateLinks() {
         this.navLinks.forEach((link, index) => {
             link.style.animation
                 ? (link.style.animation = "")
-                : (link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s');
+                : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3
+                    }s`);
         });
     }
     handleClick() {
-        this.navList.calssList.toggle(this.activeClass);
+        this.navList.classList.toggle(this.activeClass);
         this.mobileMenu.classList.toggle(this.activeClass);
         this.animateLinks();
     }
     addClickEvent() {
         this.mobileMenu.addEventListener("click", this.handleClick);
     }
-    Init() {
+    init() {
         if (this.mobileMenu) {
             this.addClickEvent();
         }
@@ -30,7 +31,7 @@ class MobileNavbar {
 }
 const mobileNavbar = new MobileNavbar(
     ".mobile-menu",
-    ".nav-list li",
+    ".nav-list",
     ".nav-list li",
 );
-mobileNavbar.Init();
+mobileNavbar.init();
